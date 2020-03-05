f84288d8a02dff0bf5fb1ea6bc1a83d93393ee6f
PierreWCS <pierrelegrain45@gmail.com>
2020-03-05 16:56:09 +0100

    admin page responsive OK

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./Admin.css";
 import Axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBirthdayCake,
-  faCross,
-  faWindowClose
-} from "@fortawesome/free-solid-svg-icons";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 import NewProduct from "./NewProduct";
+import useWindowDimensions from "../services/useWindowDimensions";
+import {Link} from "react-router-dom";
 
 const Admin = () => {
+  const { width } = useWindowDimensions();
   const [products, setProducts] = useState(null);
   const [messages, setMessages] = useState(null);
   const [displayChoice, setDisplayChoice] = useState("products");
@@ -28,37 +27,39 @@ const Admin = () => {
       });
   };
 
-  const deleteProduct = (productId) => {
-    if (window.confirm('Do you really wanna delete this product ?')) {
+  const deleteProduct = productId => {
+    if (window.confirm("Do you really wanna delete this product ?")) {
       Axios.delete(`http://localhost:8000/api/products/:${productId}`)
         .then(res => {
           if (res.status === 200) {
-            alert("Product has been deleted")
+            alert("Product has been deleted");
           } else {
-            alert("Error")
+            alert("Error");
           }
           console.log(res);
         })
         .then(response => {
-          console.log(response)
+          console.log(response);
         });
       document.location.reload();
     }
   };
 
-  const deleteMessage = (messageId) => {
-    if (window.confirm('Do you really wanna delete this message ?')) {
+  const deleteMessage = messageId => {
+    if (window.confirm("Do you really wanna delete this message ?")) {
       Axios.delete(`http://localhost:8000/api/messages/:${messageId}`)
         .then(res => {
           if (res.status === 200) {
-            alert("Message has been deleted, please refresh the page to see the changes")
+            alert(
+              "Message has been deleted, please refresh the page to see the changes"
+            );
           } else {
-            alert("Error")
+            alert("Error");
           }
           console.log(res);
         })
         .then(response => {
-          console.log(response)
+          console.log(response);
         });
       document.location.reload();
     }
@@ -76,79 +77,131 @@ const Admin = () => {
   return (
     <div className="adminMainContainer">
       <h1 className="titleAdminPage">Admin panel</h1>
-
       <div className="adminNavBar">
-        <h2 onClick={() => setDisplayChoice('products')} className={`itemNavBarAdmin ${displayChoice === 'products' ? 'activeItemAdmin' : null}`}>Products</h2>
-        <h2 onClick={() => setDisplayChoice('messages')} className={`itemNavBarAdmin ${displayChoice === 'messages' ? 'activeItemAdmin' : null}`}>Messages</h2>
+        <h2
+          onClick={() => setDisplayChoice("products")}
+          className={`itemNavBarAdmin ${
+            displayChoice === "products" ? "activeItemAdmin" : null
+          }`}
+        >
+          Products
+        </h2>
+        <h2
+          onClick={() => setDisplayChoice("messages")}
+          className={`itemNavBarAdmin ${
+            displayChoice === "messages" ? "activeItemAdmin" : null
+          }`}
+        >
+          Messages
+        </h2>
       </div>
+      <Link className="backtoHomeAdminContainer" to="/">
+        <img src={require('../images/logoBeer.png')} className="logoBackToHome" alt=""/>Back to the site
+      </Link>
 
       <div className="productsAdmin">
         {/*     I am displaying products by default and on click on messages i'm changing the content     */}
-        {products && messages
-          ?
-          displayChoice === 'products' ?
-            (
-              <div>
-                <NewProduct />
-                {
-                  displayChoice === 'products' ? <h1 className="manageAdmin">Manage your products</h1> : <h1 className="manageAdmin">New messages</h1>
-                }
-                <div className="productsContainerAdmin">
-                  {products.map(product => {
-                    return (
-                      <div className="productCardAdmin">
-                        <div onClick={() => deleteProduct(product.id)} className="deleteProductAdmin">
-                          <FontAwesomeIcon
-                            icon={faWindowClose}
-                            className="closeIconAdmin fa-2x"
-                          />
-                          <p>Delete this product</p>
-                        </div>
-                        <h3 className="itemMessageCardAdmin">Name: <span className="contentMessageAdmin">{product.name}</span></h3>
-                        <p className="itemMessageCardAdmin">Price: <span className="contentMessageAdmin">{product.price} €</span></p>
-                        <p className="itemMessageCardAdmin">Description:</p>
-                        <p>{product.description}</p>
-                        <img
-                          className="imageProductAdmin"
-                          src={require(`../images/beers_products/${product.image}`)}
-                          alt="product"
+        {products && messages ? (
+          displayChoice === "products" ? (
+            <div>
+              <NewProduct />
+              {displayChoice === "products" ? (
+                <h1 className="manageAdmin">Manage your products</h1>
+              ) : (
+                <h1 className="manageAdmin">New messages</h1>
+              )}
+              <div className="productsContainerAdmin">
+                {products.map(product => {
+                  return (
+                    <div className="productCardAdmin">
+                      <div
+                        onClick={() => deleteProduct(product.id)}
+                        className="deleteProductAdmin"
+                      >
+                        <FontAwesomeIcon
+                          icon={faWindowClose}
+                          className="closeIconAdmin fa-2x"
                         />
+                        <p>Delete this product</p>
                       </div>
-                    );
-                  })}
-                </div>
+                      <h3 className="itemMessageCardAdmin">
+                        Name:{" "}
+                        <span className="contentMessageAdmin">
+                          {product.name}
+                        </span>
+                      </h3>
+                      <p className="itemMessageCardAdmin">
+                        Price:{" "}
+                        <span className="contentMessageAdmin">
+                          {product.price} €
+                        </span>
+                      </p>
+                      <p className="itemMessageCardAdmin">Description:</p>
+                      <p>{product.description}</p>
+                      <img
+                        className="imageProductAdmin"
+                        src={require(`../images/beers_products/${product.image}`)}
+                        alt="product"
+                      />
+                    </div>
+                  );
+                })}
               </div>
-            )
-            :
-            (
-              <div className="messagesMainContainerAdmin">
-                {
-                  displayChoice === 'products' ? <h1 className="manageAdmin">Manage your products</h1> : <h1 className="manageAdmin">New messages</h1>
-                }
-                <div className="messagesContainerAdmin">
-                  {messages.map((message) => {
-                    return (
-                      <div className="messageCardAdmin">
-                        <div onClick={() => deleteMessage(message.id)} className="deleteProductAdmin">
-                          <FontAwesomeIcon
-                            icon={faWindowClose}
-                            className="closeIconAdmin fa-2x"
-                          />
-                          <p>Delete this message</p>
-                        </div>
-                        <h2>Title: <span className="contentMessageAdmin">{message.title}</span></h2>
-                        <p className="itemMessageCardAdmin">Mail: <span className="contentMessageAdmin">{message.mail}</span></p>
-                        <p className="itemMessageCardAdmin">Name: <span className="contentMessageAdmin">{message.name}</span></p>
-                        <p className="itemMessageCardAdmin">Firstname : <span className="contentMessageAdmin">{message.firstname}</span></p>
-                        <p className="itemMessageCardAdmin">Message :</p>
-                        <p className="contentMessageAdmin">{message.body}</p>
+            </div>
+          ) : (
+            <div className="messagesMainContainerAdmin">
+              {displayChoice === "products" ? (
+                <h1 className="manageAdmin">Manage your products</h1>
+              ) : (
+                <h1 className="manageAdmin">New messages</h1>
+              )}
+              <div className="messagesContainerAdmin">
+                {messages.map(message => {
+                  return (
+                    <div className="messageCardAdmin">
+                      <div
+                        onClick={() => deleteMessage(message.id)}
+                        className="deleteProductAdmin"
+                      >
+                        <FontAwesomeIcon
+                          icon={faWindowClose}
+                          className="closeIconAdmin fa-2x"
+                        />
+                        <p>Delete this message</p>
                       </div>
-                    )
-                  })}
-                </div>
+                      <h2>
+                        Title:{" "}
+                        <span className="contentMessageAdmin">
+                          {message.title}
+                        </span>
+                      </h2>
+                      <p className="itemMessageCardAdmin">
+                        Mail:{" "}
+                        <span className="contentMessageAdmin">
+                          {message.mail}
+                        </span>
+                      </p>
+                      <p className="itemMessageCardAdmin">
+                        Name:{" "}
+                        <span className="contentMessageAdmin">
+                          {message.name}
+                        </span>
+                      </p>
+                      <p className="itemMessageCardAdmin">
+                        Firstname :{" "}
+                        <span className="contentMessageAdmin">
+                          {message.firstname}
+                        </span>
+                      </p>
+                      <p className="itemMessageCardAdmin">Message :</p>
+                      <p className="contentMessageAdmin">{message.body}</p>
+                    </div>
+                  );
+                })}
               </div>
-            )
-          : null}
+            </div>
+          )
+        ) : null}
       </div>
     </div>
   );
--- src/components/NavBar/NavBarMobile.js
+++ src/components/NavBar/NavBarMobile.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import {NavLink} from "react-router-dom";
-import './NavBarMobile.css';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./NavBarMobile.css";
 
 const NavBarMobile = () => {
   const [toggle, setToggle] = useState(false);
@@ -11,26 +11,52 @@ const NavBarMobile = () => {
 
   return (
     <div>
-      <div className={`${toggle ? 'listNavBar' : 'closedMenuBurger'}`}>
+      <div className={`${toggle ? "listNavBar" : "closedMenuBurger"}`}>
         <div className="burgerMenuContainer">
-          <div onClick={() => setToggle(!toggle)} className={`burgerMenu ${toggle ? 'change' : null}`}>
-            <div className="bar1"/>
-            <div className="bar2"/>
-            <div className="bar3"/>
+          <div
+            onClick={() => setToggle(!toggle)}
+            className={`burgerMenu ${toggle ? "change" : null}`}
+          >
+            <div className="bar1" />
+            <div className="bar2" />
+            <div className="bar3" />
           </div>
         </div>
-        {
-          toggle ?
-            <div className="listNavBarMobile">
-              <NavLink activeClassName="activeItemNavBar" className="itemNavBarMobile" to="/about">ABOUT US</NavLink>
-              <NavLink activeClassName="activeItemNavBar" className="itemNavBarMobile" to="/admin">ADMIN</NavLink>
-              <NavLink activeClassName="activeItemNavBar" className="itemNavBarMobile" to="/cart">CART</NavLink>
-            </div>
-            : null
-        }
+        {toggle ? (
+          <div className="listNavBarMobile">
+            <Link to="/" className="logoAndNameNavBar">
+              <img
+                className="logoNavBar"
+                src={require("../images/logoBeer.png")}
+                alt="logo"
+              />
+            </Link>
+            <NavLink
+              activeClassName="activeItemNavBar"
+              className="itemNavBarMobile"
+              to="/about"
+            >
+              ABOUT US
+            </NavLink>
+            <NavLink
+              activeClassName="activeItemNavBar"
+              className="itemNavBarMobile"
+              to="/admin"
+            >
+              ADMIN
+            </NavLink>
+            <NavLink
+              activeClassName="activeItemNavBar"
+              className="itemNavBarMobile"
+              to="/cart"
+            >
+              CART
+            </NavLink>
+          </div>
+        ) : null}
       </div>
     </div>
-  )
+  );
 };
 
 export default NavBarMobile;
