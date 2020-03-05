01c056af4d32edc6dea9f6bf1d44d6c58f59d265
PierreWCS <pierrelegrain45@gmail.com>
2020-03-05 11:40:19 +0100

    about us page and responsive OK

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,34 +1,158 @@
-import React from "react";
+import React, { useState } from "react";
 import "./About.css";
 import Footer from "../Footer/Footer";
+import Axios from "axios";
+import { Redirect } from "react-router-dom";
 
 const About = () => {
+  const [messageTitle, setMessageTitle] = useState(null);
+  const [messageMail, setMessageMail] = useState(null);
+  const [messageName, setMessageName] = useState(null);
+  const [messageFirstName, setMessageFirstName] = useState(null);
+  const [messageBody, setMessageBody] = useState(null);
+  const [redirection, setRedirection] = useState(false);
+
+  const handleChangeTitle = event => {
+    setMessageTitle(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleChangeMail = event => {
+    setMessageMail(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleChangeName = event => {
+    setMessageName(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleChangeFirstName = event => {
+    setMessageFirstName(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleChangeBody = event => {
+    setMessageBody(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const addNewMessage = () => {
+    let url = "http://localhost:8000/api/messages";
+    if (
+      messageTitle &&
+      messageMail &&
+      messageName &&
+      messageFirstName &&
+      messageBody
+    ) {
+      Axios({
+        method: "post",
+        url: url,
+        data: {
+          title: messageTitle,
+          mail: messageMail,
+          name: messageName,
+          firstname: messageFirstName,
+          body: messageBody
+        }
+      })
+        .then(response => console.log(response))
+        .catch(error => {
+          console.log(error);
+        });
+      alert("Your message has been sent");
+      setRedirection(true);
+    } else alert("Fields need to be completed");
+  };
+
   return (
     <div className="aboutMainContainer">
       <div className="headerAbout" />
+      <div className="aboutUsContainerText">
+        <img
+          className="breweryAboutUs"
+          src={require("../images/breweryAboutUsSince.jpg")}
+          alt=""
+        />
+        <div className="textAboutUsSmallContainer">
+          <h1 className="titleAboutUsText">Wild Beers</h1>
+          <h3 className="smallSubtitleAbout">Since 2019</h3>
+          <p className="textHistoryAbout">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+            ut sollicitudin nunc, eu tempor augue. Aenean lobortis suscipit purus
+            fermentum hendrerit. Nunc posuere pellentesque libero eget mollis. Sed
+            diam ante, varius a pellentesque vel, auctor sit amet neque. Duis
+            facilisis lectus sit amet purus maximus, ut mattis magna fermentum.
+            Mauris ultrices justo dolor, in congue mi aliquam vel. Donec
+            pellentesque libero ut dolor ultricies auctor.{" "}
+          </p>
+        </div>
+      </div>
 
       {/*       Contact       */}
 
       <div className="formSectionAbout">
         <h1 className="contactUsTitle">Contact us</h1>
         <form className="formContainerAbout">
+          <label htmlFor="title">
+            <input
+              onChange={handleChangeTitle}
+              required
+              placeholder="title..."
+              className="titleInput"
+              type="text"
+              id="title"
+            />
+          </label>
           <div className="nameFirstNameContainer">
             <label htmlFor="name">
-              <input required placeholder="name..." className="nameAndFirstNameInput" type="text" id="name" />
+              <input
+                onChange={handleChangeName}
+                required
+                placeholder="name..."
+                className="nameAndFirstNameInput"
+                type="text"
+                id="name"
+              />
             </label>
             <label htmlFor="firstName">
-              <input required placeholder="firstname..." className="nameAndFirstNameInput" type="text" id="firstName" />
+              <input
+                onChange={handleChangeFirstName}
+                required
+                placeholder="firstname..."
+                className="nameAndFirstNameInput"
+                type="text"
+                id="firstName"
+              />
             </label>
           </div>
           <label htmlFor="email">
-            <input required placeholder="email..." className="mailInputAbout" type="mail" id="email" />
+            <input
+              onChange={handleChangeMail}
+              required
+              placeholder="email..."
+              className="mailInputAbout"
+              type="mail"
+              id="email"
+            />
           </label>
           <label htmlFor="message">
-            <input required placeholder="message..." className="messageInputAbout" type="text" id="message" />
+            <input
+              onChange={handleChangeBody}
+              required
+              placeholder="message..."
+              className="messageInputAbout"
+              type="text"
+              id="message"
+            />
           </label>
         </form>
-        <button className="aboutUsButton formButton">Send</button>
+        <button onClick={addNewMessage} className="aboutUsButton formButton">
+          Send
+        </button>
       </div>
+      {redirection ? <Redirect to="/" /> : null}
       <Footer />
     </div>
   );
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -42,6 +42,7 @@ const Admin = () => {
         .then(response => {
           console.log(response)
         });
+      document.location.reload();
     }
   };
 
@@ -59,6 +60,7 @@ const Admin = () => {
         .then(response => {
           console.log(response)
         });
+      document.location.reload();
     }
   };
 
