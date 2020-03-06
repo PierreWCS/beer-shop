c1ce7f84d872217bb0b0c57f4b1f7798e9cfdee0
PierreWCS <pierrelegrain45@gmail.com>
2020-03-06 16:41:59 +0100

    fix product adding and success message

--- src/components/Admin/NewProduct.js
+++ src/components/Admin/NewProduct.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './NewProduct.css';
+import React, { useState } from "react";
+import "./NewProduct.css";
 import Axios from "axios";
 
 const NewProduct = () => {
@@ -9,36 +9,35 @@ const NewProduct = () => {
   const [productDescription, setProductDescription] = useState(null);
   const [productAlcohol, setProductAlcohol] = useState(null);
 
-  const handleChangeName = (event) => {
+  const handleChangeName = event => {
     setProductName(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleChangePrice = (event) => {
+  const handleChangePrice = event => {
     setProductPrice(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleChangeImage = (event) => {
+  const handleChangeImage = event => {
     setProductImage(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleChangeDescription = (event) => {
+  const handleChangeDescription = event => {
     setProductDescription(event.target.value);
     console.log(event.target.value);
   };
 
-  const handleChangeAlcohol = (event) => {
+  const handleChangeAlcohol = event => {
     setProductAlcohol(event.target.value);
     console.log(event.target.value);
   };
 
-
   const addNewProduct = () => {
-    let url = 'http://localhost:8000/api/products';
+    let url = "http://localhost:8000/api/products";
     Axios({
-      method: 'post',
+      method: "post",
       url: url,
       data: {
         name: productName,
@@ -48,10 +47,15 @@ const NewProduct = () => {
         alcohol: productAlcohol
       }
     })
-      .then((response) => console.log(response))
-      .catch((error) => {
-        console.log(error);
+      .then(response => {
+        console.log(response);
+        alert("The product has been added");
+        document.location.reload();
       })
+      .catch(error => {
+        console.log(error);
+        alert("Something has failed during creation of the product");
+      });
   };
 
   return (
@@ -60,28 +64,68 @@ const NewProduct = () => {
       <form className="formFakeShop">
         <label className="labelNewProduct" htmlFor="addProductFakeShopName">
           Name of the product :
-          <input onChange={handleChangeName} required pattern="[A-Z][A-Za-z' -]+" className="inputShopForm" placeholder="Not heineken" type="text" id="addProductFakeShopName"/>
+          <input
+            onChange={handleChangeName}
+            required
+            pattern="[A-Z][A-Za-z' -]+"
+            className="inputShopForm"
+            placeholder="Not heineken"
+            type="text"
+            id="addProductFakeShopName"
+          />
         </label>
         <label className="labelNewProduct" htmlFor="addProductFakeShopPrice">
           Price :
-          <input onChange={handleChangePrice} required className="inputShopForm" placeholder="3.10" id="addProductFakeShopPrice"/>
+          <input
+            onChange={handleChangePrice}
+            required
+            className="inputShopForm"
+            placeholder="3.10"
+            id="addProductFakeShopPrice"
+          />
         </label>
         <label className="labelNewProduct" htmlFor="addProductFakeShopImage">
           Image url :
-          <input onChange={handleChangeImage} required size="30" className="inputShopForm" placeholder="asahi.png" type="text" id="addProductFakeShopPrice"/>
+          <input
+            onChange={handleChangeImage}
+            required
+            size="30"
+            className="inputShopForm"
+            placeholder="asahi.png"
+            type="text"
+            id="addProductFakeShopPrice"
+          />
         </label>
         <label className="labelNewProduct" htmlFor="addProductFakeShopImage">
           Description
-          <input onChange={handleChangeDescription} required size="90" className="inputShopForm" placeholder="Very smooth beer" type="text" id="addProductFakeShopPrice"/>
+          <input
+            onChange={handleChangeDescription}
+            required
+            size="90"
+            className="inputShopForm"
+            placeholder="Very smooth beer"
+            type="text"
+            id="addProductFakeShopPrice"
+          />
         </label>
         <label className="labelNewProduct" htmlFor="addProductFakeShopImage">
           Alcohol
-          <input onChange={handleChangeAlcohol} required size="30" className="inputShopForm" placeholder="5.3" type="text" id="addProductFakeShopPrice"/>
-          <p onClick={addNewProduct} className="addNewProductButton">Add</p>
+          <input
+            onChange={handleChangeAlcohol}
+            required
+            size="30"
+            className="inputShopForm"
+            placeholder="5.3"
+            type="text"
+            id="addProductFakeShopPrice"
+          />
+          <p onClick={addNewProduct} className="addNewProductButton">
+            Add
+          </p>
         </label>
       </form>
     </div>
-  )
+  );
 };
 
 export default NewProduct;
